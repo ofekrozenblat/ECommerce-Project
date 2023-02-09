6418b1a73cf71c9b5f7e17c9c47edf350b4d1aeb
Ofek Rozenblat <20247892+ofekrozenblat@users.noreply.github.com>
2023-02-08 23:10:36 -0500

    Small change

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -16,18 +16,18 @@ public abstract class Dao {
 	
 	public void save(Model model) {
 		// Save should check if user is being created or is already created
-		
 		Map<String, String> attributes = model.getAttributeMap();
+		String table = model.getTable();
 		for(String attribute : attributes.keySet()) {
-			String table = model.getTable();
+			
 			// construct save query
 		}
 	}
 	
 	public void delete(Model model) {
 		Map<String, String> attributes = model.getAttributeMap();
+		String table = model.getTable();
 		for(String attribute : attributes.keySet()) {
-			String table = model.getTable();
 			// construct delete query
 		}
 	}
