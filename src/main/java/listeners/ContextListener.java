69ac979af771113a9321a62eca454ba722c8614f
Ofek Rozenblat <20247892+ofekrozenblat@users.noreply.github.com>
2023-02-17 20:35:39 -0500

    Clean up

--- src/main/java/listeners/ContextListener.java
+++ src/main/java/listeners/ContextListener.java
@@ -18,33 +18,5 @@ public class ContextListener implements ServletContextListener{
 	  @Override
 	  public void contextInitialized(ServletContextEvent arg0) {
 		  // Do stuff on server start up
-		  
-		  // TESTING
-//		  User user = ModelFactory.createUser();
-//		  user.setFirstName("Bobby3");
-//		  try {
-//			user.save();
-//			System.out.println("ID: " + user.getId());
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		  
-		  UserDao userDao = new UserDao();
-		  User user;
-		try {
-			user = userDao.get(18);
-			System.out.println(user.getFirstName());
-			user.setFirstName("Kool");
-			user.save();
-			System.out.println(user.getFirstName());
-			//user.delete();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		  
-		  
-		  
 	  }
 }
