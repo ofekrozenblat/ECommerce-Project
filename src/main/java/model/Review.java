ce795ab25cba12e21f28aec8b723de21ee8fe582
Ofek Rozenblat <20247892+ofekrozenblat@users.noreply.github.com>
2023-02-28 17:13:46 -0500

    Create Review.java

---
+++ src/main/java/model/Review.java
@@ -0,0 +1,87 @@
+package model;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+
+import dao.Dao;
+
+public class Review extends Model {
+
+	public static final String table = "reviews";
+	public static final String primaryKeyColumnName = "id";
+	
+	public Review(Dao dao) {
+		super(dao);
+		
+		// Set the date of this review to the current date it was created
+		setDate(Calendar.getInstance());
+	}
+	
+	public Review(Dao dao, int id) {
+		super(dao, id);
+	}
+
+	@Override
+	public String getPrimaryKeyColumnName() {
+		return primaryKeyColumnName;
+	}
+
+	@Override
+	public String getTable() {
+		return table;
+	}
+
+	@Override
+	protected String[] getAttributeNames() {
+		return new String[] {"rating", "description", "date", "item_id", "user_id"};
+	}
+	
+	// Getters & Setters
+	public void setRating(int rating) {
+	    setAttribute("name", String.valueOf(rating));
+	}
+
+	public int getRating() {
+	    return Integer.parseInt(getAttribute("rating"));
+	}
+	
+	public void setDescription(String description) {
+	    setAttribute("description", description);
+	}
+
+	public String getDescription() {
+	    return getAttribute("description");
+	}
+	
+	public void setDate(Calendar calendar) {
+		setAttribute("date", calendar.toString());
+	}
+	
+	public Calendar getDate() throws ParseException {
+		String dateString = getAttribute("date");
+		DateFormat df = DateFormat.getDateInstance();
+		Date date = df.parse(dateString);
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		return calendar;
+	}
+	
+	public void setItemId(int ItemId) {
+		setAttribute("item_id", String.valueOf(ItemId));
+	}
+	
+	public int getItemId() {
+		return Integer.parseInt(getAttribute("item_id"));
+	}
+	
+	public void setUserId(int userId) {
+		setAttribute("user_id", String.valueOf(userId));
+	}
+	
+	public int getUserId() {
+		return Integer.parseInt(getAttribute("user_id"));
+	}
+
+}
