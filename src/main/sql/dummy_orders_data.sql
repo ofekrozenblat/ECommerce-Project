c28a5a98007c6170d8d2382abba8d87dd0f21d67
Ofek Rozenblat <20247892+ofekrozenblat@users.noreply.github.com>
2023-03-03 14:57:45 -0500

    Created dummy data

---
+++ src/main/sql/dummy_orders_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO orders (total, user_id, billing_address_id, payment_id)
+VALUES (100.00, 1, 1, 1);
+
+INSERT INTO orders (total, user_id, billing_address_id, payment_id)
+VALUES (250.75, 1, 2, 2);
+
+INSERT INTO orders (total, user_id, billing_address_id, payment_id)
+VALUES (75.50, 1, 3, 3);
