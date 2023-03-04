01959ed2ae3b37d90a12d6f7c665d27f3025b675
Ofek Rozenblat <20247892+ofekrozenblat@users.noreply.github.com>
2023-03-03 20:20:45 -0500

    Authentication for secure password management

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -2,6 +2,9 @@ package dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
 import model.User;
 
 /**
@@ -44,6 +47,46 @@ public class UserDao extends Dao {
 		}
 		
 		return user;
+	}
+	
+	public String getStoredPassword(int userId) throws SQLException {
+		String storedPassword = "";
+		String table = "passwords";
+		String condition = "user_id" + "=" + userId;
+		String[] conditions = {condition};
+		ResultSet resultSet;
+		
+		try {
+			resultSet = connection.executeSelect(table, null, conditions);
+		} catch (SQLException e) {
+			throw new SQLException("Failed to retreive password for user with id " + userId
+					+ ": " + e.getMessage());
+		}
+		
+		try {
+			resultSet.next();
+			storedPassword = resultSet.getString("password");
+		} catch (SQLException e) {
+			throw new SQLException("No password attribute for user with id " + userId + " found"
+					+ ": " + e.getMessage());
+		}
+		
+		return storedPassword;
+	}
+	
+	public void storePassword(String password, int userId) throws SQLException {
+		String table = "passwords";
+		String primaryKeyColumn = "id";
+		Map<String, String> attributes = new HashMap<String, String>();
+		attributes.put("password", password);
+		attributes.put("user_id", String.valueOf(userId));
+		
+		// Executes the insert and gets the ID of the new primary key
+		int retrievedPrimaryKey = connection.executeInsert(table, primaryKeyColumn, attributes);
+		
+		if (retrievedPrimaryKey == -1) {
+			throw new SQLException("Failed to store password for user with id " + userId);
+		}
 	}
 	
 	// Can create other specific methods such as getAllUsers(), getUsersBy(...), etc..
---
+++ src/main/java/tests/auth/AuthenticatorTest.java
@@ -0,0 +1,45 @@
+package tests.auth;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import auth.Authenticator;
+import hthurow.tomcatjndi.TomcatJNDI;
+
+class AuthenticatorTest {
+
+	private static TomcatJNDI tomcatJNDI;
+
+    @BeforeAll
+    static void setup() {
+		tomcatJNDI = new TomcatJNDI();
+		tomcatJNDI.processContextXml(new File("src\\main\\webapp\\META-INF\\context.xml"));
+		tomcatJNDI.processWebXml(new File("src\\main\\webapp\\WEB-INF\\web.xml"));
+		tomcatJNDI.start();
+    }
+
+    @AfterAll
+    static void tearDown() {
+    	tomcatJNDI.tearDown();
+    }
+
+	@Test
+	void testRegister() {
+		try {
+			String password = "hello123";
+			Authenticator.registerUser(password, 1);
+			
+			assertTrue(Authenticator.validateUser(password, 1));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Error: " + e.getMessage());
+		}
+	}
+
+}
