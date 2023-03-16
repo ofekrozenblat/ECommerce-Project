82c46405e39efdb4ca172b8b09d14c6732f23157
hussain-fatmi <hfatmi12@my.yorku.ca>
2023-03-16 17:36:48 -0400

    Created tests

---
+++ src/main/java/tests/RecommendationTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+class RecommendationTest {
+	
+	@Test
+    void testRecommendations() {
+    	
+    }
+}
---
+++ src/main/java/tests/filters/FiltersTest.java
@@ -0,0 +1,26 @@
+package tests.filters;
+
+import org.junit.jupiter.api.Test;
+
+class FiltersTest {
+	
+	@Test
+    void testCategoryFilters() {
+    	
+    }
+	
+	@Test
+    void testBrandFilters() {
+		
+	}
+	
+	@Test
+    void testColorFilters() {
+		
+	}
+	
+	@Test
+    void testPriceFilters() {
+		
+	}
+}
