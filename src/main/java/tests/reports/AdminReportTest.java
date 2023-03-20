fd6891232e904e5576c0b75aa1570bf199be2ef9
hussain-fatmi <hfatmi12@my.yorku.ca>
2023-03-19 20:58:57 -0400

    Created AdminReport Tests

---
+++ src/main/java/tests/reports/AdminReportTest.java
@@ -0,0 +1,25 @@
+package tests.reports;
+
+import java.io.File;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import hthurow.tomcatjndi.TomcatJNDI;
+
+public class AdminReportTest {
+	private static TomcatJNDI tomcatJNDI;
+
+    @BeforeAll
+    static void setup() {
+		tomcatJNDI = new TomcatJNDI();
+		tomcatJNDI.processContextXml(new File("src\\main\\webapp\\META-INF\\context.xml"));
+		tomcatJNDI.processWebXml(new File("src\\main\\webapp\\WEB-INF\\web.xml"));
+		tomcatJNDI.start();
+    }
+
+    @AfterAll
+    static void tearDown() {
+    	tomcatJNDI.tearDown();
+    }
+}
