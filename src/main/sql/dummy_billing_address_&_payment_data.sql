5bab99477184fc07c34e4d74c1fee52b85d4216d
Ofek Rozenblat <20247892+ofekrozenblat@users.noreply.github.com>
2023-03-24 15:49:28 -0400

    SQL injection test

--- src/main/sql/dummy_billing_address_&_payment_data.sql
+++ src/main/sql/dummy_billing_address_&_payment_data.sql
@@ -4,6 +4,6 @@ VALUES ('123 Main St', 'Ontario', 'Canada', 'M4S2M4'),
        ('789 Oak St', 'Alberta', 'Canada', 'T6E2G2');
        
 INSERT INTO payments (payment_type, credit_card_name, credit_card_number, credit_card_expiration, credit_card_cvv)
-VALUES (1, 'John Smith', '1234567812345678', '2024-06-01', '123'),
-       (2, 'Jane Doe', '2345678923456789', '2023-11-01', '456'),
-       (3, 'Bob Johnson', '3456789034567890', '2025-03-01', '789');
+VALUES (1, 'John Smith', '1234567812345678', '01/24', '123'),
+       (2, 'Jane Doe', '2345678923456789', '11/23', '456'),
+       (3, 'Bob Johnson', '3456789034567890', '03/25', '789');
